8539a21b54d817fcfa35c90ae6fdf6f3f76aef4f
svasquezs1 <104176586+svasquezs1@users.noreply.github.com>
2022-10-05 20:02:41 -0500

    Actualizacion

--- entrega #2/entrega2.py
+++ entrega #2/entrega2.py
@@ -1,54 +1,58 @@
 import geopandas as gpd
-	import pandas as pd
-	import matplotlib.pyplot as plt
-	from shapely import wkt
+import pandas as pd
+import matplotlib.pyplot as plt
+from shapely import wkt
 	
 
 	#Load area
-	area = pd.read_csv('poligono_de_medellin.csv',sep=';')
-	area['geometry'] = area['geometry'].apply(wkt.loads)
-	area = gpd.GeoDataFrame(area)
+area = pd.read_csv('poligono_de_medellin.csv',sep=';')
+area['geometry'] = area['geometry'].apply(wkt.loads)
+area = gpd.GeoDataFrame(area)
 	
 
 	#Load streets
-	edges = pd.read_csv('calles_de_medellin_con_acoso.csv',sep=';')
-	edges['geometry'] = edges['geometry'].apply(wkt.loads)
-	edges = gpd.GeoDataFrame(edges)
+edges = pd.read_csv('calles_de_medellin_con_acoso.csv',sep=';')
+edges['geometry'] = edges['geometry'].apply(wkt.loads)
+edges = gpd.GeoDataFrame(edges)
 	
 
+
 	#Create plot
-	fig, ax = plt.subplots(figsize=(12,8))
+fig, ax = plt.subplots(figsize=(12,8))
 	
 
 	# Plot the footprint
-	area.plot(ax=ax, facecolor='black')
+area.plot(ax=ax, facecolor='black')
 	
 
 	# Plot street edges
-	edges.plot(ax=ax, linewidth=0.3, column='harassmentRisk', legend=True, missing_kwds={'color': 'dimgray'})
+edges.plot(ax=ax, linewidth=0.3, column='harassmentRisk', legend=True, missing_kwds={'color': 'dimgray'})
 	
 
-	plt.title("Riesgo de acoso en las calles de Medellín")
-	plt.tight_layout()
-	plt.savefig("mapa-riesgo-de-acoso.png")
+plt.title("Riesgo de acoso en las calles de Medellín")
+plt.tight_layout()
+plt.savefig("mapa-riesgo-de-acoso.png")
 	
-
+ 
 	#lifeQuality.plot(ax=ax,column=fraction_of_males, legend=True)
 	#lifeQuality.plot(ax=ax,column=fraction_under_1_salary, legend=True)
 	#lifeQuality.plot(ax=ax,column='harassmentRisk', legend=True)
 	
 
-	fig, ax = plt.subplots(figsize=(12,8))
+fig, ax = plt.subplots(figsize=(12,8))
 	
 
 	# Plot the footprint
-	area.plot(ax=ax, facecolor='black')
+area.plot(ax=ax, facecolor='black')
 	
 
 	# Plot street edges
-	edges.plot(ax=ax, linewidth=0.3, column='length', legend=True, missing_kwds={'color': 'dimgray'})
+edges.plot(ax=ax, linewidth=0.3, column='length', legend=True, missing_kwds={'color': 'dimgray'})
 	
 
-	plt.title("Longitud en metros de las calles de Medellín")
-	plt.tight_layout()
-	plt.savefig("mapa-de-called-con-longitud.png")
+plt.title("Longitud en metros de las calles de Medellín")
+plt.tight_layout()
+plt.savefig("mapa-de-called-con-longitud.png")
+
+#--Aprender más diccionario--
+#{Llave=origen:{destino1:(distancia,riesgo),destino2:(distancia,riesgo)},origen2:{}}
